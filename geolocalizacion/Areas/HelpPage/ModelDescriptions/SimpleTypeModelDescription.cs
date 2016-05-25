6954996570a25fadaf938dd3f3e24fc9d8adf19c
Adrian Vergara <adrianvergara22@gmail.com>
2016-05-25 01:16:51 -0500

    Api Terminada

---
+++ geolocalizacion/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace geolocalizacion.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ geolocalizacion/Models/Persona.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace geolocalizacion.Models
+{
+    public class Persona
+    {
+        [Key]
+        public int idPersona { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string tipo_identificacion { get; set; }
+        public int identificacion { get; set; }
+        public string regimen { get; set; }
+        public string tipo_persona { get; set; }
+        public string estado_civil { get; set; }
+        public string sexo { get; set; }
+        public double latitud { get; set; }
+        public double longitud { get; set; }
+    }
+}
